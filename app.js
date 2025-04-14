05f4f3a374fa22f05be3b96af60c88f7f382af93
seramosgadsoft <sergioramos@adsoft.company>
2025-04-14 17:54:44 -0500

    Add main application entry point with Express setup

---
+++ app.js
@@ -0,0 +1,21 @@
+import express, { json } from 'express'
+import { createMovieRouter } from './routes/movies.js'
+import { corsMiddleware } from './middlewares/cors.js'
+import 'dotenv/config'
+
+// This is the main entry point of the application
+export const createApp = ({ movieModel }) => {
+  const app = express()
+  app.use(json())
+  app.use(corsMiddleware())
+  app.disable('x-powered-by')
+
+  // This is where we define our routes
+  app.use('/movies', createMovieRouter({ movieModel }))
+
+  const PORT = process.env.PORT ?? 1234
+
+  app.listen(PORT, () => {
+    console.log(`server listening on port http://localhost:${PORT}`)
+  })
+}
